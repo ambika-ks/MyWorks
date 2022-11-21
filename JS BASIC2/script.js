3bd5a866d365ed34c12f14619e9e0b1ce45092e4
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2022-11-21 14:30:30 +0000

    A Basic javaScript to display Student Name and age in two ways in the console

---
+++ JS BASIC2/script.js
@@ -0,0 +1,8 @@
+//Using assignment operator to assign values
+studentName="jan";
+studentAge=34;
+console.log(`Student Name: ${studentName}, Age: ${studentAge}!`);
+//Using concatenation operator
+console.log("My Name is : "+studentName);
+console.log("student Name :  "+studentName + ", Age :" +studentAge + "!");
+
