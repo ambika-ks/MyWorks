9293ec8c4b9ef357773b85cca7f49d8d3f7c5b6a
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2022-11-21 14:33:40 +0000

    A Basic javaScript to display different datatypes in the console

---
+++ JS BASIC3/script.js
@@ -0,0 +1,18 @@
+
+// A string is a series of characters and is surrounded by quotes
+var myString=" Welcome to Java Script";
+console.log(`myString : ${myString}`);
+var mypwd= "kdhd,fdlfjdsj67382";
+console.log(`My Password is : ${mypwd}`);
+
+//A number can be either integer or a decimal
+var numInt= 100;
+console.log(`My Integer Number is : ${numInt}`);
+var numDecimal=110.234;
+console.log(`My Decimal number is : ${numDecimal}`);
+
+//Booleans have two value:- true/false
+var isBooleanTrue=true;
+var isBooleanFalse=false;
+console.log(`Boolean True value  : ${isBooleanTrue}`);
+console.log(`Boolean False value :${isBooleanFalse}`);
