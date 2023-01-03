2eb888372f81ff9ce3796d043b000e76ba21f926
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-01-03 16:50:26 +0000

    Objects and methods

---
+++ OBJECT METHODS/script.js
@@ -0,0 +1,25 @@
+//WRITE YOUR CODE BELOW
+var menu={
+    coffee: 4.50,
+    latte: 6.50,
+    coldBrew: 6,
+    motcha:7
+};
+
+//Assign menu values to the prices variable
+var prices =Object.values(menu);
+
+//Initialize total amount
+var total=0;
+
+//Print the entire menu
+console.log(` Drinks Available 
+    ${Object.keys(menu)}`);
+
+//Find total amount with loop statement
+for(let i=0;i<prices.length;i++){
+ total+=prices[i];
+}
+
+//total cost
+console.log(`Total Menu Cost : ${total}`);
