14e9c5b0b9935420a0a38c98ac68295ad022db1d
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-01-03 18:02:04 +0000

    Added comments to the Window Objects

---
+++ WINDOW OBJECT/README.md
@@ -0,0 +1,13 @@
+# 📐 Add Comments to Implementation of Window Object
+
+Add comments in describing the functionality of the code found in [ Window Object Activity Files](WINDOW OBJECT/script.js).
+
+## 📝 Notes
+
+Refer to the documentation: 
+
+[MDN Web Docs on Window](https://developer.mozilla.org/en-US/docs/Web/API/Window)
+
+[MDN Web Docs on Document](https://developer.mozilla.org/en-US/docs/Web/API/Document)
+
+---
---
+++ WINDOW OBJECT/script.js
@@ -0,0 +1,17 @@
+// TODO: What will the following line of code log?
+// The Window.console property returns a reference to the console object, which provides methods for logging information to the browser's console. 
+// These methods are intended for debugging purposes only and should not be relied on for presenting information to end users.
+console.log(window);
+
+// TODO: What will the following line of code log?
+// window.document returns a reference to the document contained in the window.
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+// Document.documentElement returns the Element that is the root element of the document (for example, the <html> element for HTML documents).
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+// The head read-only property of the Document interface returns the <head> element of the current document.
+console.log(document.head);
+
